d3e1f9034b3a676624a3f762dd6cd668c2bf7b52
Matthew Harrison-Jones <matthojo@gmail.com>
2012-01-27 16:19:57 +0000

    Added settings and high scores

--- js/script.js
+++ js/script.js
@@ -18,179 +18,214 @@ Array.prototype.removeByValue = function(val) {
 }
 
 $(document).ready(function() {
- var fps = 0, now, lastUpdate = (new Date)*1 - 1;
  
- // The higher this value, the less the FPS will be affected by quick changes
- // Setting this to 1 will show you the FPS of the last sampled frame only
- var fpsFilter = 50;
+ // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+ // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
  
- var canvas = $("#myCanvas");
- var context = canvas.get(0).getContext("2d");
- var canvasWidth = $(window).get(0).innerWidth;
- var canvasHeight = $(window).get(0).innerHeight;
- var refreshRate = 77;
+ // requestAnimationFrame polyfill by Erik Möller
+ // fixes from Paul Irish and Tino Zijdel
  
- var playGame = true;
- var muted = false;
- var score = 0;
- var scoreOut = document.getElementById('score');
- var stars;
- var numstars;
- var enemies;
- var numEnemies;
+ (function() {
+     var lastTime = 0;
+     var vendors = ['ms', 'moz', 'webkit', 'o'];
+     for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+         window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+         window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                    || window[vendors[x]+'CancelRequestAnimationFrame'];
+     }
+  
+     if (!window.requestAnimationFrame)
+         window.requestAnimationFrame = function(callback, element) {
+             var currTime = new Date().getTime();
+             var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+             var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+               timeToCall);
+             lastTime = currTime + timeToCall;
+             return id;
+         };
+  
+     if (!window.cancelAnimationFrame)
+         window.cancelAnimationFrame = function(id) {
+             clearTimeout(id);
+         };
+ }());
  
- var powerups;
- var numPowerups;
- var maxNumPowerups;
- var powerupCharge;
- var powerupChargeRate;
- var powerupCharged;
- 
- var canX;
- var canY;
- var touchable = 'ontouchstart' in window || 'createTouch' in document;
- //if(touchable) refreshRate = 35;
- if(touchable) muted = true;
- var touches = []; // array of touch vectors
- var rightKey = false;
- var leftKey = false;
- var upKey = false;
- var downKey = false;
- var space = false;
- 
- var shootSound = $("#shootSound").get(0);
- //var thrustersSound = $("#thrustersSound").get(0);
- var destroySound = $("#destroySound").get(0);
- var powerupSound = $("#powerupSound").get(0);
- var hitSound = $("#hitSound").get(0);
- 
- 
- var Star = function(x,y){
- 	this.x = x,
- 	this.y = y,
- 	this.brightness = Math.floor(Math.random()*4);
- 	this.radius = Math.floor(Math.random()*4);
- }
- var Bullet = function(owner){
- 	this.x = owner.x,
- 	this.y = owner.y,
- 	this.rotation = owner.rotation,
- 	this.vx = 0,
- 	this.vy = 0,
- 	this.speed = 10,
- 	this.size = 3,
- 	this.lifetime = 800,
- 	this.owner = owner
- }
- 
- var Powerup = function(x,y){
- 	this.x = x,
- 	this.y = y,
- 	this.size = 8,
- 	this.lifetime = 8000,
- 	this.type = randomFromTo(1,4);
- }
- var Ship = function(x, y) {
-   this.x = x,
-   this.y = y,
-   this.width = 20,
-   this.height = 20,
-   this.halfWidth = this.width/2,
-   this.halfHeight = this.height/2,
-   this.vx = 0,
-   this.vy = 0,
-   this.speed = 0,
-   this.defaultMaxSpeed = 4,
-   this.maxSpeed = 4,
-   this.energy = 360,
-   this.energyRate = 18,
-   this.charge = 0,
-   this.defaultChargeRate = 10,
-   this.chargeRate = 10,
-   this.health = 100,
-   this.elasticity = 0.5,
-   this.rotation = 0,
-   this.rotationSpeed = 2,
-   this.flying = false,
-   this.powerup = false,
-   this.resetPowerupTime = 2000,
-   this.powerupTime = 2000,
-   this.shield = false,
-   this.superSpeed = false,
-   this.superFire = false
- };
- 
- var ShipEnemy = function(x, y) {
-   this.x = x,
-   this.y = y,
-   this.width = 20,
-   this.height = 20,
-   this.halfWidth = this.width/2,
-   this.halfHeight = this.height/2,
-   this.vx = 0,
-   this.vy = 0,
-   this.speed = 0,
-   this.defaultMaxSpeed = 2,
-   this.maxSpeed = 2,
-   this.energy = 360,
-   this.energyRate = 18,
-   this.charge = 0,
-   this.chargeRate = 10,
-   this.defaultChargeRate = 10,
-   this.health = 10,
-   this.reactionTime = 20,
-   this.accuracy = 20,
-   this.elasticity = 0.5,
-   this.rotation = 0,
-   this.rotationSpeed = 4,
-   this.flying = false
- };
- 
- stars = new Array();
- numStars = 40;
- 
- for (var i=0;i<numStars; i++){
- 	var x = Math.floor(Math.random()*canvasWidth);
- 	var y = Math.floor(Math.random()*canvasHeight);
- 	stars.push(new Star(x,y))
- }
- while(stars.length < numStars){
- 	var x = canvasWidth+20+Math.floor(Math.random()*canvasWidth);
- 	var y = Math.floor(Math.random()*canvasHeight);
- 	stars.push(new Star(x,y));
- }
- 
- enemies = new Array();
- numEnemies = 1;
-
- for (var i=0;i<numEnemies; i++){
- 	var x = canvasWidth+20+Math.floor(Math.random()*canvasWidth);
- 	var y = Math.floor(Math.random()*canvasHeight);
- 	enemies.push(new ShipEnemy(x,y));
- }
- 
- ship = new Ship(150, canvas.height()/2);
- 
- bullets = new Array();
- numBullets = 0;
- 
- powerups = new Array();
- numPowerups = 1;
- maxNumPowerups = 2;
- powerupCharge = 200;
- powerupChargeRate = 1;
- powerupChargeMax = 1000;
- 
- for (var i=0;i<numPowerups; i++){
- 	var x = Math.floor(Math.random()*canvasWidth);
- 	var y = Math.floor(Math.random()*canvasHeight);
- 	powerups.push(new Powerup(x,y));
- }
- 
- function innit(){
+	 var fps = 0, now, lastUpdate = (new Date)*1 - 1;
+	 
+	 // The higher this value, the less the FPS will be affected by quick changes
+	 // Setting this to 1 will show you the FPS of the last sampled frame only
+	 var fpsFilter = 50;
+	 
+	 var canvas = $("#myCanvas");
+	 var footerHeight = $("footer").height();
+	 var context = canvas.get(0).getContext("2d");
+	 var canvasWidth = $(window).get(0).innerWidth;
+	 var canvasHeight = $(window).get(0).innerHeight-footerHeight;
+	 var refreshRate = 77;
+	 
+	 var playGame = true;
+	 var muted = false;
+	 var score = 0;
+	 var highScore = 0;
+	 var scoreOut = document.getElementById('score');
+	 var highScoreOut = document.getElementById('highScore');
+	 var stars;
+	 var numstars;
+	 var enemies;
+	 var numEnemies;
+	 
+	 var powerups;
+	 var numPowerups;
+	 var maxNumPowerups;
+	 var powerupCharge;
+	 var powerupChargeRate;
+	 var powerupCharged;
+	 
+	 var canX;
+	 var canY;
+	 var touchable = 'ontouchstart' in window || 'createTouch' in document;
+	 //if(touchable) refreshRate = 35;
+	 if(touchable) muted = true;
+	 var touches = []; // array of touch vectors
+	 var rightKey = false;
+	 var leftKey = false;
+	 var upKey = false;
+	 var downKey = false;
+	 var space = false;
+	 
+	 var shootSound = $("#shootSound").get(0);
+	 //var thrustersSound = $("#thrustersSound").get(0);
+	 var destroySound = $("#destroySound").get(0);
+	 var powerupSound = $("#powerupSound").get(0);
+	 var hitSound = $("#hitSound").get(0);
+	 
+	 
+	 var Star = function(x,y){
+	 	this.x = x,
+	 	this.y = y,
+	 	this.brightness = Math.floor(Math.random()*4);
+	 	this.radius = Math.floor(Math.random()*4);
+	 }
+	 var Bullet = function(owner){
+	 	this.x = owner.x,
+	 	this.y = owner.y,
+	 	this.rotation = owner.rotation,
+	 	this.vx = 0,
+	 	this.vy = 0,
+	 	this.speed = 10,
+	 	this.size = 3,
+	 	this.lifetime = 800,
+	 	this.owner = owner
+	 }
+	 
+	 var Powerup = function(x,y){
+	 	this.x = x,
+	 	this.y = y,
+	 	this.size = 8,
+	 	this.lifetime = 8000,
+	 	this.type = randomFromTo(1,4);
+	 }
+	 var Ship = function(x, y) {
+	   this.x = x,
+	   this.y = y,
+	   this.width = 20,
+	   this.height = 20,
+	   this.halfWidth = this.width/2,
+	   this.halfHeight = this.height/2,
+	   this.vx = 0,
+	   this.vy = 0,
+	   this.speed = 0,
+	   this.defaultMaxSpeed = 4,
+	   this.maxSpeed = 4,
+	   this.energy = 360,
+	   this.energyRate = 18,
+	   this.charge = 0,
+	   this.defaultChargeRate = 10,
+	   this.chargeRate = 10,
+	   this.health = 100,
+	   this.elasticity = 1,
+	   this.rotation = 0,
+	   this.rotationSpeed = 2,
+	   this.flying = false,
+	   this.powerup = false,
+	   this.resetPowerupTime = 2000,
+	   this.powerupTime = 2000,
+	   this.shield = false,
+	   this.superSpeed = false,
+	   this.superFire = false
+	 };
+	 
+	 var ShipEnemy = function(x, y) {
+	   this.x = x,
+	   this.y = y,
+	   this.width = 20,
+	   this.height = 20,
+	   this.halfWidth = this.width/2,
+	   this.halfHeight = this.height/2,
+	   this.vx = 0,
+	   this.vy = 0,
+	   this.speed = 0,
+	   this.defaultMaxSpeed = 2,
+	   this.maxSpeed = 2,
+	   this.energy = 360,
+	   this.energyRate = 18,
+	   this.charge = 0,
+	   this.chargeRate = 10,
+	   this.defaultChargeRate = 10,
+	   this.health = 10,
+	   this.reactionTime = 20,
+	   this.accuracy = 20,
+	   this.elasticity = 1,
+	   this.rotation = 0,
+	   this.rotationSpeed = 4,
+	   this.flying = false
+	 };
+	 
+	 stars = new Array();
+	 numStars = 40;
+	 
+	 for (var i=0;i<numStars; i++){
+	 	var x = Math.floor(Math.random()*canvasWidth);
+	 	var y = Math.floor(Math.random()*canvasHeight);
+	 	stars.push(new Star(x,y))
+	 }
+	 while(stars.length < numStars){
+	 	var x = canvasWidth+20+Math.floor(Math.random()*canvasWidth);
+	 	var y = Math.floor(Math.random()*canvasHeight);
+	 	stars.push(new Star(x,y));
+	 }
+	 
+	 enemies = new Array();
+	 numEnemies = 1;
+	
+	 for (var i=0;i<numEnemies; i++){
+	 	var x = canvasWidth+20+Math.floor(Math.random()*canvasWidth);
+	 	var y = Math.floor(Math.random()*canvasHeight);
+	 	enemies.push(new ShipEnemy(x,y));
+	 }
+	 
+	 ship = new Ship(150, canvas.height()/2);
+	 updateHighScore();
+	 
+	 bullets = new Array();
+	 numBullets = 0;
+	 
+	 powerups = new Array();
+	 numPowerups = 1;
+	 maxNumPowerups = 2;
+	 powerupCharge = 200;
+	 powerupChargeRate = 1;
+	 powerupChargeMax = 1000;
+	 
+	 for (var i=0;i<numPowerups; i++){
+	 	var x = Math.floor(Math.random()*canvasWidth);
+	 	var y = Math.floor(Math.random()*canvasHeight);
+	 	powerups.push(new Powerup(x,y));
+	 }
+ function draw(){
  
  	var canvasWidth = $(window).get(0).innerWidth;
- 	var canvasHeight = $(window).get(0).innerHeight;
+ 	var canvasHeight = $(window).get(0).innerHeight-footerHeight;
  	window.scrollTo(0,0);
 
  	while(enemies.length < numEnemies){
@@ -465,19 +500,21 @@ $(document).ready(function() {
  	var bulletsLength = bullets.length;
  	for (var i=0;i<bulletsLength;i++){
  		var tmpBullet = bullets[i];
- 			
- 			context.save();
- 			context.translate(tmpBullet.x, tmpBullet.y);
- 			context.rotate((tmpBullet.rotation) * Math.PI/180);
- 			context.translate(-tmpBullet.x, -tmpBullet.y);
- 		 	context.fillStyle = '#FFF';
- 			context.beginPath();
- 			context.fillRect(tmpBullet.x,tmpBullet.y,tmpBullet.size*4,tmpBullet.size); 
- 			context.closePath();
- 			context.fill();
- 			context.restore();
- 		animateBullet(tmpBullet, i);
- 		
+ 			try {
+	 			context.save();
+	 			context.translate(tmpBullet.x, tmpBullet.y);
+	 			context.rotate((tmpBullet.rotation) * Math.PI/180);
+	 			context.translate(-tmpBullet.x, -tmpBullet.y);
+	 		 	context.fillStyle = '#FFF';
+	 			context.beginPath();
+	 			context.fillRect(tmpBullet.x,tmpBullet.y,tmpBullet.size*4,tmpBullet.size); 
+	 			context.closePath();
+	 			context.fill();
+	 			context.restore();
+	 			animateBullet(tmpBullet, i);
+ 			}catch (error) {
+ 				// Do nothing
+ 			} 		
  	}
 	ship.flying = false;
 	
@@ -571,17 +608,31 @@ function animateEnemy(tmpEnemy) {
 
  }
  function animateBullet(bullet, i){
+ 	
+ 	if(bullet.lifetime <= 0){
+ 		bullets.removeByValue(bullet);
+ 	}
+ 	else if(bullet.lifetime > 0) bullet.lifetime = bullet.lifetime - 10;
+ 	
+ 	if (bullets.y >= canvasHeight - bullets.size) {
+ 	  bullets.removeByValue(bullet);
+ 	}
+ 	if (bullets.x >= canvasWidth - bullets.size ) {
+ 	  bullets.removeByValue(bullet);
+ 	}
+ 	if (bullets.y <= 0 + bullets.size) {
+ 	  bullets.removeByValue(bullet);
+ 	}
+ 	if (bullets.x <= 0 + bullets.size) {
+ 	  bullets.removeByValue(bullet);
+ 	}
+ 	
  	bullet.vy = Math.sin(bullet.rotation*Math.PI/180) * bullet.speed;
  	bullet.vx = Math.cos(bullet.rotation*Math.PI/180) * bullet.speed;
  	
  	bullet.x += bullet.vx;
  	bullet.y += bullet.vy;
  	
- 	if(bullet.lifetime > 0) bullet.lifetime = bullet.lifetime - 10;
- 	
- 	if(bullet.lifetime <= 0){
- 		bullets.removeByValue(bullet);
- 	}
  	if(bullet.owner == ship){
 	 	for(i=0;i<enemies.length;i++){
 	 		var tmpEnemy = enemies[i];
@@ -707,11 +758,34 @@ function animateEnemy(tmpEnemy) {
  			destroySound.currentTime = 0;
  			destroySound.play();
 		}
- 		score--;
- 		scoreOut.innerHTML = "Score: "+score;
+ 		//score--;
+ 		endGame();
+ 	}
+ }
+ function endGame(){ 	
+ 	saveHighScore();
+ 	updateHighScore();
+ 	score = 0;
+ 	scoreOut.innerHTML = "Score: "+score;
+ }
+ function saveHighScore(){
+ 	if (Modernizr.localstorage)
+ 	{
+ 		if(score > highScore){
+ 	    	localStorage.setItem('highScore', score);
+ 	    }
+ 	}
+ }
+ function updateHighScore(){
+ 	if(Modernizr.localstorage && localStorage.getItem('highScore')){
+ 		highScoreOut.innerHTML = "High Score: "+localStorage.getItem('highScore');
+ 		highScore = localStorage.getItem('highScore');
+ 	}else{
+ 		highScoreOut.innerHTML = "High Score: 0";
  	}
  }
  function onKeyDown(evt) {
+   evt.preventDefault();
    if (evt.keyCode == 39) rightKey = true;
    else if (evt.keyCode == 37) leftKey = true;
    if (evt.keyCode == 38) upKey = true;
@@ -730,17 +804,23 @@ function animateEnemy(tmpEnemy) {
  function onTouchStart(e) {
  	 e.preventDefault();
  	 touches = e.touches;
-   upKey = true;
+   	 upKey = true;
  }
  function onTouchMove(e) {
      e.preventDefault();
      touches = e.touches;
  }
  function onTouchEnd(e) {
- 		touches = e.touches;
-		upKey = false;
+ 	touches = e.touches;
+	upKey = false;
  }
- 
+ $('#toggleSound').toggle(function() {
+   muted = true;
+   $(this).addClass('muted');
+ }, function() {
+   muted = false;
+   $(this).removeClass('muted');
+ });
  function randomFromTo(from, to){
         return Math.floor(Math.random() * (to - from + 1) + from);
  }
@@ -751,8 +831,8 @@ function animateEnemy(tmpEnemy) {
    	document.body.addEventListener( 'touchstart', onTouchStart, false );
    	document.body.addEventListener( 'touchmove', onTouchMove, false );
    	document.body.addEventListener( 'touchend', onTouchEnd, false );
-   	window.onorientationchange = innit;  
-   	window.onresize = innit;
+   	window.onorientationchange = draw;  
+   	window.onresize = draw;
    document.body.addEventListener("touchcancel", onTouchEnd, false);
    }else{
    	$(document).keydown(onKeyDown);
@@ -761,13 +841,21 @@ function animateEnemy(tmpEnemy) {
    }
  });
  
- $(window).resize(innit);
+ $(window).resize(draw);
  
- setInterval(function() {
+ /*setInterval(function() {
  	if(playGame){
- 		innit();
+ 		draw();
  	}
- } , 1000 / refreshRate);
+ } , 1000 / refreshRate);*/
+  
+ function startGame() {
+     requestAnimationFrame(startGame);
+     draw();
+ 
+ }
+ startGame();
+ 
  var fpsOut = document.getElementById('fps');
  setInterval(function(){
    fpsOut.innerHTML = fps.toFixed(1) + "fps";
