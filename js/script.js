c73c8d279b06a3fe8aac6a52096fb8881705dd06
Matthew Harrison-Jones <matthojo@gmail.com>
2012-02-07 22:34:47 +0000

    Added menus

--- js/script.js
+++ js/script.js
@@ -2,39 +2,6 @@
 
 $(document).ready(function () {
 
-    // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-    // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
-
-    // requestAnimationFrame polyfill by Erik Möller
-    // fixes from Paul Irish and Tino Zijdel
-
-    (function () {
-        var lastTime = 0;
-        var vendors = ['ms', 'moz', 'webkit', 'o'];
-        for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-            window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-            window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
-                || window[vendors[x] + 'CancelRequestAnimationFrame'];
-        }
-
-        if (!window.requestAnimationFrame) window.requestAnimationFrame = function (callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function () {
-                    callback(currTime + timeToCall);
-                },
-                timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
-
-        if (!window.cancelAnimationFrame) {
-            window.cancelAnimationFrame = function (id) {
-                clearTimeout(id);
-            };
-        }
-    }());
-
     /* FPS monitoring
      *
      * The higher the 'fpsFilter' value, the less the FPS will be affected by quick changes
@@ -42,40 +9,24 @@ $(document).ready(function () {
      */
 
     // stats.js r9 - http://github.com/mrdoob/stats.js
-    var Stats=function(){var h,a,r=0,s=0,i=Date.now(),u=i,t=i,l=0,n=1E3,o=0,e,j,f,b=[[16,16,48],[0,255,255]],m=0,p=1E3,q=0,d,k,g,c=[[16,48,16],[0,255,0]];h=document.createElement("div");h.style.cursor="pointer";h.style.width="80px";h.style.opacity="0.9";h.style.zIndex="10001";h.addEventListener("mousedown",function(a){a.preventDefault();r=(r+1)%2;0==r?(e.style.display="block",d.style.display="none"):(e.style.display="none",d.style.display="block")},!1);e=document.createElement("div");e.style.textAlign=
+    var Stats =function(){var h,a,r=0,s=0,i=Date.now(),u=i,t=i,l=0,n=1E3,o=0,e,j,f,b=[[16,16,48],[0,255,255]],m=0,p=1E3,q=0,d,k,g,c=[[16,48,16],[0,255,0]];h=document.createElement("div");h.style.cursor="pointer";h.style.width="80px";h.style.opacity="0.9";h.style.zIndex="10001";h.addEventListener("mousedown",function(a){a.preventDefault();r=(r+1)%2;0==r?(e.style.display="block",d.style.display="none"):(e.style.display="none",d.style.display="block")},!1);e=document.createElement("div");e.style.textAlign=
         "left";e.style.lineHeight="1.2em";e.style.backgroundColor="rgb("+Math.floor(b[0][0]/2)+","+Math.floor(b[0][1]/2)+","+Math.floor(b[0][2]/2)+")";e.style.padding="0 0 3px 3px";h.appendChild(e);j=document.createElement("div");j.style.fontFamily="Helvetica, Arial, sans-serif";j.style.fontSize="9px";j.style.color="rgb("+b[1][0]+","+b[1][1]+","+b[1][2]+")";j.style.fontWeight="bold";j.innerHTML="FPS";e.appendChild(j);f=document.createElement("div");f.style.position="relative";f.style.width="74px";f.style.height=
         "30px";f.style.backgroundColor="rgb("+b[1][0]+","+b[1][1]+","+b[1][2]+")";for(e.appendChild(f);74>f.children.length;)a=document.createElement("span"),a.style.width="1px",a.style.height="30px",a.style.cssFloat="left",a.style.backgroundColor="rgb("+b[0][0]+","+b[0][1]+","+b[0][2]+")",f.appendChild(a);d=document.createElement("div");d.style.textAlign="left";d.style.lineHeight="1.2em";d.style.backgroundColor="rgb("+Math.floor(c[0][0]/2)+","+Math.floor(c[0][1]/2)+","+Math.floor(c[0][2]/2)+")";d.style.padding=
         "0 0 3px 3px";d.style.display="none";h.appendChild(d);k=document.createElement("div");k.style.fontFamily="Helvetica, Arial, sans-serif";k.style.fontSize="9px";k.style.color="rgb("+c[1][0]+","+c[1][1]+","+c[1][2]+")";k.style.fontWeight="bold";k.innerHTML="MS";d.appendChild(k);g=document.createElement("div");g.style.position="relative";g.style.width="74px";g.style.height="30px";g.style.backgroundColor="rgb("+c[1][0]+","+c[1][1]+","+c[1][2]+")";for(d.appendChild(g);74>g.children.length;)a=document.createElement("span"),
         a.style.width="1px",a.style.height=30*Math.random()+"px",a.style.cssFloat="left",a.style.backgroundColor="rgb("+c[0][0]+","+c[0][1]+","+c[0][2]+")",g.appendChild(a);return{getDomElement:function(){return h},getFps:function(){return l},getFpsMin:function(){return n},getFpsMax:function(){return o},getMs:function(){return m},getMsMin:function(){return p},getMsMax:function(){return q},update:function(){i=Date.now();m=i-u;p=Math.min(p,m);q=Math.max(q,m);k.textContent=m+" MS ("+p+"-"+q+")";var a=Math.min(30,
         30-30*(m/200));g.appendChild(g.firstChild).style.height=a+"px";u=i;s++;if(i>t+1E3)l=Math.round(1E3*s/(i-t)),n=Math.min(n,l),o=Math.max(o,l),j.textContent=l+" FPS ("+n+"-"+o+")",a=Math.min(30,30-30*(l/100)),f.appendChild(f.firstChild).style.height=a+"px",t=i,s=0}}};
-
-    var stats = new Stats();
-
-    // Align top-left
-    stats.getDomElement().style.position = 'absolute';
-    stats.getDomElement().style.left = '0px';
-    stats.getDomElement().style.top = '0px';
-
-    document.body.appendChild(stats.getDomElement());
-
-    setInterval(function () {
-
-        stats.update();
-
-    }, 1000 / 60);
-
-
+	
     // Canvas Settings
     var canvas = $("#myCanvas");
     var footerHeight = $("footer").height();
     var context = canvas.get(0).getContext("2d");
     var canvasWidth = $(window).get(0).innerWidth;
     var canvasHeight = $(window).get(0).innerHeight - footerHeight;
-    window.scrollTo(0, 0);
 
     // Game settings
-    var playGame = true, muted = false;
-    var score = 0, highScore = 0, scoreOut = $("#score"), highScoreOut = $("#highScore");
+    var playGame = false, pause = false, gameOver = false, muted = false, debug = false;
+    var uiStart = $(".start"), uiPause = $(".paused"), uiOver = $(".gameOver"), playButton = $(".playGame");
+    var score = 0, highScore = 0, scoreOut = $("#score"), overScoreOut = $(".scored"), highScoreOut = $(".highScore"), newhighScore = $(".newHighScore");
     var stars, numStars;
     var enemies, numEnemies;
     var powerups, numPowerups;
@@ -203,12 +154,6 @@ $(document).ready(function () {
         var y = Math.floor(Math.random() * canvasHeight);
         stars.push(new Star(x, y))
     }
-    while (stars.length < numStars) {
-        var x = canvasWidth + 20 + Math.floor(Math.random() * canvasWidth);
-        var y;
-        y = Math.floor(Math.random() * canvasHeight);
-        stars.push(new Star(x, y));
-    }
 
     enemies = new Array();
     numEnemies = 1;
@@ -242,7 +187,10 @@ $(document).ready(function () {
 
         var canvasWidth = $(window).get(0).innerWidth;
         var canvasHeight = $(window).get(0).innerHeight - footerHeight;
-
+        canvas.attr("width", canvasWidth);
+        canvas.attr("height", canvasHeight);
+	    window.scrollTo(0, 0);
+	    
         if (enemies.length < numEnemies) {
             var x = Math.floor(Math.random() * canvasWidth);
             var y = Math.floor(Math.random() * canvasHeight);
@@ -265,17 +213,10 @@ $(document).ready(function () {
         }
         if (powerupCharge < powerupChargeMax && numPowerups != maxNumPowerups) powerupCharge += powerupChargeRate;
 
-        canvas.attr("width", canvasWidth);
-        canvas.attr("height", canvasHeight);
-
         context.save();
-        //context.fillStyle = '#12141A';
         context.clearRect(0, 0, canvas.width(), canvas.height());
         context.restore();
 
-        // 1 - apply velocity to position (vx -> x)
-        //ship.x += ship.vx;
-        //ship.y += ship.vy;
         if (touchable) {
             if (touches.length > 0) {
                 var touch = touches[0];
@@ -359,7 +300,6 @@ $(document).ready(function () {
         if (ship.flying) {
             context.fillStyle = "#f98224";
             context.beginPath();
-            //context.arc(0, 0, 2, 0, Math.PI * 2, true);
             context.moveTo(ship.x - randomFromTo(ship.halfWidth, ship.width), ship.y); // give the (x,y) coordinates
             context.lineTo(ship.x - (ship.halfWidth - 1), ship.y + (ship.halfHeight / 2));
             context.lineTo(ship.x - (ship.halfWidth - 1), ship.y - (ship.halfHeight / 2));
@@ -592,6 +532,7 @@ $(document).ready(function () {
         player.vx = 0;
         player.vy = 0;
         player.health = 100;
+        newhighScore.hide();
         resetPowerups(player);
     }
 
@@ -775,15 +716,9 @@ $(document).ready(function () {
                 dx = tmpEnemy.x - bullet.x;
                 dy = tmpEnemy.y - bullet.y;
                 distance = Math.sqrt((dx * dx) + (dy * dy));
-                //console.log(distance);
                 if (distance < tmpEnemy.width) {
-                    //playGame = false;
                     destroy(tmpEnemy, bullet);
                     bullets.removeByValue(bullet);
-
-                    //console.log("destroyed");
-                    //playGame = false;
-
                 }
 
             }
@@ -791,7 +726,6 @@ $(document).ready(function () {
             dx = ship.x - bullet.x;
             dy = ship.y - bullet.y;
             distance = Math.sqrt((dx * dx) + (dy * dy));
-            //console.log(distance);
             if (distance <= (ship.width * 4) && ship.shield.active) {
                 bullets.removeByValue(bullet);
             }
@@ -922,7 +856,6 @@ $(document).ready(function () {
 
     function destroy(player, by) {
         player.health = player.health - 10;
-        //console.log(player.health);
         if (player.health == 0 && player != ship) {
             if (!muted) {
                 destroySound.currentTime = 0;
@@ -931,7 +864,7 @@ $(document).ready(function () {
             enemies.removeByValue(player);
             collisions.push(new Collision(player.x, player.y, by.rotation));
             score++;
-            scoreOut.text(score);
+            scoreOut.html(score);
             levelUp();
         } else if (player.health == 0 && player == ship) {
             resetPlayer(player);
@@ -995,14 +928,20 @@ $(document).ready(function () {
         saveHighScore();
         updateHighScore();
         numEnemies = 1;
+        var tmpScore = score;
         score = 0;
-        scoreOut.text(score);
+        scoreOut.html(score);
+        overScoreOut.html(tmpScore);
+        playGame = false;
+        gameOver = true;
     }
 
     function saveHighScore() {
         if (Modernizr.localstorage) {
             if (score > highScore) {
                 localStorage.setItem('highScore', score);
+                newhighScore.show();
+
             }
         }
     }
@@ -1012,7 +951,7 @@ $(document).ready(function () {
             highScoreOut.text("High Score: " + localStorage.getItem('highScore'));
             highScore = localStorage.getItem('highScore');
         } else {
-            highScoreOut.text("High Score: 0");
+            highScoreOut.html("High Score: 0");
         }
     }
 
@@ -1023,6 +962,8 @@ $(document).ready(function () {
         if (evt.keyCode == 38) upKey = true;
         else if (evt.keyCode == 40) downKey = true;
         if (evt.keyCode == 32) space = true;
+        if(evt.keyCode == 112) debugMode();
+        if(evt.keyCode == 80) if(!pause)pause=true;
 
     }
 
@@ -1070,6 +1011,27 @@ $(document).ready(function () {
             }
         }
     }
+    
+	function debugMode() {
+
+        if(!debug){
+
+            var stats = new Stats();
+
+            // Align top-left
+            stats.getDomElement().style.position = 'absolute';
+            stats.getDomElement().style.left = '0px';
+            stats.getDomElement().style.top = '0px';
+
+            document.body.appendChild(stats.getDomElement());
+            setInterval(function () {
+                stats.update();
+            }, 1000 / 60);
+            debug = true;
+
+        }
+	}
+        
 
     $('#toggleSound').toggle(function () {
         muted = true;
@@ -1088,6 +1050,15 @@ $(document).ready(function () {
         $(this).removeClass('fullscreen_exit_alt');
         $(this).addClass('fullscreen_alt');
     });
+    
+    playButton.on("click touchend",(function(event){
+    	playGame = true;
+        pause = false;
+        gameOver = false;
+    	uiStart.hide();
+    	uiPause.hide();
+    	uiOver.hide();
+    }));
 
     function randomFromTo(from, to) {
         return Math.floor(Math.random() * (to - from + 1) + from);
@@ -1128,11 +1099,19 @@ $(document).ready(function () {
 
     function startGame() {
         requestAnimationFrame(startGame);
-        draw();
+        if(playGame){
+            if(pause){
+                uiPause.show();
+            }else{
+        	    draw();
+            }
+        }else if(gameOver){
+        	uiOver.show();
+        }else{
+        	uiStart.show();
+        }
     }
-
+    draw();
     startGame();
 
 });
-
-
